ab5973f30d1887380c6af3acb0b0db3e1380d3ad
Bubble-droid <Bubble-droid@github.com>
2025-07-14 13:51:14 +0800

    Update

--- src/handlers/messagesHandler/newChatMemberHandler.js
+++ src/handlers/messagesHandler/newChatMemberHandler.js
@@ -34,7 +34,7 @@ async function newChatMemberHandler(message, env) {
 		'  - 请确保你当前使用的 GUI.for.Cores 版本，与所选内核版本兼容。（默认情况下 GUI.for.Cores 客户端与最新版内核保持同步）',
 		'  - 如遇到更新后仍无法解决的问题，提问时应直接发出现问题的完整窗口截图，说明你使用的是哪个客户端和版本，以及使用的内核版本，还有进行什么操作时遇到的问题。',
 		`  - 有关 GUI.for.Cores 和内核的问题，都可以 @ 智能助手（\`${botName}\`）提问，以获得及时解答。`,
-	].join('\n');
+	].join('\n\n');
 
 	try {
 		const { message_id } = await bot.sendMessage({
