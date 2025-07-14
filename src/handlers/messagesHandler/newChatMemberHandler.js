4eeb5c2ce7a12e49a9c4a63b15cd064ead3de977
Bubble-droid <Bubble-droid@github.com>
2025-07-14 13:48:41 +0800

    Update

--- src/handlers/messagesHandler/newChatMemberHandler.js
+++ src/handlers/messagesHandler/newChatMemberHandler.js
@@ -22,17 +22,19 @@ async function newChatMemberHandler(message, env) {
 		new_chat_member: { id: newMemberId, first_name = '', last_name = '' },
 	} = message;
 
-	const newMemberFullName = `${first_name} ${last_name}`.trim();
+	const newMemberFullName = `${first_name} ${last_name}`;
 
 	const newMemberMention = `[${newMemberFullName}](tg://user?id=${newMemberId})`;
-	const welcomeText = `欢迎  ${newMemberMention}  加入 ${chatTitle} 讨论组！
-
-		* 提问前须知：
-
-		  - 遇到任何问题请先将 GUI.for.Cores 主程序，和滚动发行更新到最新版。
-		  - 请确保你当前使用的 GUI.for.Cores 版本，与所选内核版本兼容。
-		  - 提问应直接发报错或者日志截图，说明你使用的 GUI.for.Cores 客户端和版本，以及使用的内核版本。
-		  - 有关 GUI.for.Cores 操作和内核配置的问题，都可以 @ 智能助手(${botName}) 提问，以获得及时解答。`;
+	const welcomeText = [
+		`欢迎 ${newMemberMention} 加入 ${chatTitle} 讨论组！`,
+		'* 提问前须知：',
+		'  - GUI.for.Cores 分为两个客户端（GUI.for.Clash 和 GUI.for.SingBox）',
+		'  - 遇到任何问题请先将 GUI.for.Cores 主程序更新到最新版，以及安装`滚动发行`插件并运行再次更新。',
+		'  - 不要只更新 GUI.for.Cores 客户端，而不更新内核，反之依然。',
+		'  - 请确保你当前使用的 GUI.for.Cores 版本，与所选内核版本兼容。（默认情况下 GUI.for.Cores 客户端与最新版内核保持同步）',
+		'  - 如遇到更新后仍无法解决的问题，提问时应直接发出现问题的完整窗口截图，说明你使用的是哪个客户端和版本，以及使用的内核版本，还有进行什么操作时遇到的问题。',
+		`  - 有关 GUI.for.Cores 和内核的问题，都可以 @ 智能助手（\`${botName}\`）提问，以获得及时解答。`,
+	].join('\n');
 
 	try {
 		const { message_id } = await bot.sendMessage({
@@ -41,7 +43,7 @@ async function newChatMemberHandler(message, env) {
 		});
 
 		if (message_id) {
-			await scheduleDeletion(env, chatId, message_id, 3 * 60 * 1_000);
+			await scheduleDeletion(env, chatId, message_id, 5 * 60 * 1_000);
 		}
 	} catch (error) {
 		console.log('Error sending welcome message');
